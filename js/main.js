ad6272ecdc275ef6ca474a66261b0cf39eb48c2f
cherubina-pantano <cherubina.pantano@gmail.com>
2020-10-19 15:12:02 +0200

    JS Methods

--- js/main.js
+++ js/main.js
@@ -5,3 +5,18 @@ inserirlo in un array con altri cognomi: 'Bianchi', 'Rossi', 'Duzioni', 'Balsano
 stampa la lista ordinata alfabeticamente
 scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
 */
+
+var listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Giorgini'];
+
+var userSurname = prompt('Inserisci il tuo cognome');
+listaCognomi.push(userSurname);       /* <-- Aggiunge nuovo elemento alla lista */
+console.log(listaCognomi);
+
+listaCognomi.sort();                     /* <-- Ordine alfabetico */
+console.log(listaCognomi);
+
+var position = listaCognomi.indexOf(userSurname);      /* <-- Posizione dell'item nella lista (indice array) */
+console.log('indice array: ' + position);
+
+var position = listaCognomi.indexOf(userSurname) + 1;  /* <-- Posizione dell'item nella lista (indice "umano") */
+console.log('indice umano: ' + position);
