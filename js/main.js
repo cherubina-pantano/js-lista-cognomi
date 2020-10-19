42a7ea48bf7c115f45f5a8ee97880e8ad0ff60eb
cherubina-pantano <cherubina.pantano@gmail.com>
2020-10-19 14:33:21 +0200

    Add set up

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/*
+Lista Cognomi:
+Chiedere all'utente il cognome
+inserirlo in un array con altri cognomi: 'Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Giorgini'
+stampa la lista ordinata alfabeticamente
+scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+*/
